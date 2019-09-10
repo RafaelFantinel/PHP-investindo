5528db2f6fcf9d5c38cfdf51ff2f6a24122e23e1
RafaelFantinel <rafaeldeoliveirafanitnel@hotmail.com>
2019-09-10 17:10:27 -0300

    Criando menu e customizando

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -5,10 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="stylesheet" href="{{asset('css/stylesheet.css')}}">
+<link href="https://fonts.googleapis.com/css?family=Fredoka+One&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.css">
     <title>Investindo</title>
     @yield('css-view')
 </head>
 <body>
+    @include('templates.menu-lateral')
     @yield('conteudo-view')
     @yield('js-view')
 </body>
---
+++ resources/views/templates/menu-lateral.blade.php
@@ -0,0 +1,22 @@
+<nav id="principal">
+<ul class="">
+    <li class="">
+        <a href="">
+            <i class="fa fa-users"></i>
+            <h3>Usuarios</h3>
+        </a>
+    </li>
+    <li>
+        <a href="">
+            <i class="fa fa-university"></i>
+            <h3>Instituições</h3>
+        </a>
+    </li>
+    <li>
+    <a href="">
+        <i class="fa fa-object-group"></i>
+        <h3>Grupos</h3>
+    </a>
+</li>
+</ul>
+</nav>
